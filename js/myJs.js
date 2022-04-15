8cdbd602a1ae5902de5a3a62beb5d6f94a01c1ed
Nguyễn Văn Hoàng <88831612+zunohoang@users.noreply.github.com>
2022-04-15 11:49:29 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -10,7 +10,7 @@ const textConfig = {
   text9: "Vì cậu đẹp try và dthuong vlll:>>",
   text10: "Tớ biết mà ^^ Yêu cậux 1000.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com/nguyenphan299";
+            window.location = "https://www.facebook.com/JunoKatee.Programer";
           },
         });
       }
